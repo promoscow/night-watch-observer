ccfb823061124e960b53c764a062a9b247851b3c
chernyshov <chernyshov@bakapp.ru>
2019-02-26 23:04:09 +0300

    initial commit

---
+++ src/main/java/ru/xpendence/nightwatchobserver/service/ApiService.java
@@ -0,0 +1,12 @@
+package ru.xpendence.nightwatchobserver.service;
+
+/**
+ * Author: Vyacheslav Chernyshov
+ * Date: 26.02.19
+ * Time: 22:55
+ * e-mail: 2262288@gmail.com
+ */
+public interface ApiService {
+
+    void test();
+}
---
+++ src/main/java/ru/xpendence/nightwatchobserver/service/ApiServiceImpl.java
@@ -0,0 +1,20 @@
+package ru.xpendence.nightwatchobserver.service;
+
+import com.vk.api.sdk.client.VkApiClient;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Author: Vyacheslav Chernyshov
+ * Date: 26.02.19
+ * Time: 22:56
+ * e-mail: 2262288@gmail.com
+ */
+public class ApiServiceImpl implements ApiService {
+
+    @Autowired
+    private VkApiClient client;
+
+    @Override
+    public void test() {
+    }
+}
